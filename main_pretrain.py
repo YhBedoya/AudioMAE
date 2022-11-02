08fdfb1d8d10724e6e7660a44c586d6462703201
YhBedoya <yh.bv.0829@gmail.com>
2022-11-02 20:23:10 +0100

    Fix paths

--- main_pretrain.py
+++ main_pretrain.py
@@ -85,12 +85,12 @@ def get_args_parser():
                         help='epochs to warmup LR')
 
     # Dataset parameters
-    parser.add_argument('--data_path', default='/content/drive/MyDrive/Data Science and Engineering - PoliTo2/Thesis/models/mae-main/Data/', type=str,  #TODO: data dir
+    parser.add_argument('--data_path', default='/content/drive/MyDrive/Data Science and Engineering - PoliTo2/Thesis/models/AudioMAE/audioSubset/', type=str,  #TODO: data dir
                         help='dataset path')
 
-    parser.add_argument('--output_dir', default='/content/drive/MyDrive/Data Science and Engineering - PoliTo2/Thesis/models/mae-main/output_dir',
+    parser.add_argument('--output_dir', default='/content/drive/MyDrive/Data Science and Engineering - PoliTo2/Thesis/models/AudioMAE/output_dir',
                         help='path where to save, empty for no saving')
-    parser.add_argument('--log_dir', default='/content/drive/MyDrive/Data Science and Engineering - PoliTo2/Thesis/models/mae-main/output_dir',
+    parser.add_argument('--log_dir', default='/content/drive/MyDrive/Data Science and Engineering - PoliTo2/Thesis/models/AudioMAE/output_dir',
                         help='path where to tensorboard log')
     parser.add_argument('--device', default='cuda',  #TODO:cuda
                         help='device to use for training / testing')
